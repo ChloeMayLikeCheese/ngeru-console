b90b44291db0ec7e39a5061f30e20f5a3eb0794d
Chloe <125437090+ChloeMayLikeCheese@users.noreply.github.com>
2024-09-24 11:24:04 +1200

    code comments

--- ngeru/ngeru-Java/src/com/ngeru/Main.java
+++ ngeru/ngeru-Java/src/com/ngeru/Main.java
@@ -6,26 +6,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" ／l、" );
-        System.out.println("（ﾟ､ ｡７" );
-        System.out.println(" l、ﾞ~ヽ");
-        System.out.println("じしf_, )ノ ");
-        System.out.println("    ");
+        System.out.println("  ／l、\n（ﾟ､ ｡７\n l、ﾞ~ヽ\n じしf_, )ノ \n");
 
+
+//      Create scanner
         Scanner scanner = new Scanner(System.in);
+//      while loop for the input prompt and switch statement
         while (true) {
             System.out.print("\uD83D\uDC08/> ");
 
+//          Convert input to lower case
             String input = scanner.next().toLowerCase();
 
-
+//          Switch statement for input
             switch (input) {
                 case "help":
-                    System.out.println("1\n2");
+                    System.out.println("help - Display Information On Commands\n2");
                     break;
                 case "test2":
                     System.out.println("test2");
                     break;
+
 
             }
         }
